de84ef8a711c81f730c2a16da20978157e4581a0
jast90 <zhangzhiwen91@gmail.com>
2020-04-07 16:31:02 +0800

    hive UDF

---
+++ hive/src/main/java/cn/jast/hive/Lower.java
@@ -0,0 +1,13 @@
+package cn.jast.hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class Lower extends UDF{
+
+    public String evaluate(final String s){
+        if(s == null){
+            return null;
+        }
+        return s.toLowerCase();
+    }
+}
