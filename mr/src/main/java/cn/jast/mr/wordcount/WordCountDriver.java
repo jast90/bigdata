a3a3033717723b99cbc530248db27f5e33f2ff09
jast90 <zhangzhiwen91@gmail.com>
2020-04-16 13:41:55 +0800

    MR CombineTextInputFormat 案例

--- mr/src/main/java/cn/jast/mr/wordcount/WordCountDriver.java
+++ mr/src/main/java/cn/jast/mr/wordcount/WordCountDriver.java
@@ -6,7 +6,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.lib.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -26,9 +28,22 @@ public class WordCountDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+       
+        // combineTextInputFormat(job);
+        
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    /**
+     * CombineTextInputFormat 案例
+     */
+    private static void combineTextInputFormat(Job job){
+        job.setInputFormatClass(CombineTextInputFormat.class);
+        //1 KB 
+        CombineTextInputFormat.setMaxInputSplitSize(job, 1024);
+
     }
 
 }
