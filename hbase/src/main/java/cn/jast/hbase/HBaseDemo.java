a19454a340980c51f35b27fb32f0b9cb125fe8e7
jast90 <zhangzhiwen91@gmail.com>
2020-05-29 17:28:24 +0800

    Java 操作HBase

---
+++ hbase/src/main/java/cn/jast/hbase/HBaseDemo.java
@@ -0,0 +1,165 @@
+package cn.jast.hbase;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+
+
+public class HBaseDemo {
+    public static Configuration configuration;
+    public static Connection connection;
+    public static Admin admin;
+
+    public static void main(String[] args) throws IOException {
+        createTable("Score", new String[]{"sname","course"});
+       //在Score表中插入一条数据，其行键为95001,sname为Mary（因为sname列族下没有子列所以第四个参数为空）
+        //等价命令：put 'Score','95001','sname','Mary'
+        //insertRow("Score", "95001", "sname", "", "Mary");
+        //在Score表中插入一条数据，其行键为95001,course:Math为88（course为列族，Math为course下的子列）
+        //等价命令：put 'Score','95001','score:Math','88'
+        //insertRow("Score", "95001", "course", "Math", "88");
+        //在Score表中插入一条数据，其行键为95001,course:English为85（course为列族，English为course下的子列）
+        //等价命令：put 'Score','95001','score:English','85'
+        //insertRow("Score", "95001", "course", "English", "85");
+ 
+        //1、删除Score表中指定列数据，其行键为95001,列族为course，列为Math
+        //执行这句代码前请deleteRow方法的定义中，将删除指定列数据的代码取消注释注释，将删除制定列族的代码注释
+        //等价命令：delete 'Score','95001','score:Math'
+        //deleteRow("Score", "95001", "course", "Math");
+ 
+        //2、删除Score表中指定列族数据，其行键为95001,列族为course（95001的Math和English的值都会被删除）
+        //执行这句代码前请deleteRow方法的定义中，将删除指定列数据的代码注释，将删除制定列族的代码取消注释
+        //等价命令：delete 'Score','95001','score'
+        //deleteRow("Score", "95001", "course", "");
+ 
+        //3、删除Score表中指定行数据，其行键为95001
+        //执行这句代码前请deleteRow方法的定义中，将删除指定列数据的代码注释，以及将删除制定列族的代码注释
+        //等价命令：deleteall 'Score','95001'
+        //deleteRow("Score", "95001", "", "");
+ 
+        //查询Score表中，行键为95001，列族为course，列为Math的值
+        //getData("Score", "95001", "course", "Math");
+        //查询Score表中，行键为95001，列族为sname的值（因为sname列族下没有子列所以第四个参数为空）
+        //getData("Score", "95001", "sname", "");
+ 
+        //删除Score表
+        //deleteTable("Score");
+    }
+
+    public static void init() {
+        configuration = HBaseConfiguration.create();
+        configuration.set("hbase.rootdir", "hdfs://hadoop100:9000/hbase");
+        try {
+            connection = ConnectionFactory.createConnection(configuration);
+            admin = connection.getAdmin();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close() {
+        try {
+            if (admin != null) {
+                admin.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTable(String table, String[] colFamily) throws IOException {
+        init();
+        TableName tableName = TableName.valueOf(table);
+        if(admin.tableExists(tableName)){
+            System.out.println("table is exists!");
+        }else{
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            for (String col : colFamily) {
+                HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(col);
+                hTableDescriptor.addFamily(hColumnDescriptor);
+            }
+            admin.createTable(hTableDescriptor);
+            System.out.println("create table success");
+        }
+        close();
+    }
+
+
+    public static void deleteTable(String tableName) throws IOException {
+        init();
+        TableName tn = TableName.valueOf(tableName);
+        if(admin.tableExists(tn)){
+            admin.disableTable(tn);
+            admin.deleteTable(tn);
+        }
+        close();
+    }
+
+    public static void listTables() throws IOException {
+        init();
+        HTableDescriptor hTableDescriptors[] = admin.listTables();
+        for (HTableDescriptor hTableDescriptor : hTableDescriptors) {
+            System.out.println(hTableDescriptor.getNameAsString());
+        }
+        close();
+    }
+
+    public static void insertRow(String tableName,String rowKey,String colFamily,String col ,String val)
+            throws IOException {
+        init();
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Put put = new Put(rowKey.getBytes());
+        put.addColumn(colFamily.getBytes(), col.getBytes(), val.getBytes());
+        table.put(put);
+        table.close();
+        close();
+    }
+
+    public static void deleteRow(String tableName,String rowKey,String colFamily ,String col) throws IOException {
+        init();
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Delete delete = new Delete(rowKey.getBytes());
+        table.delete(delete);
+        table.close();
+        close();
+    }
+
+    public static void getData(String tableName,String rowKey,String colFamily,String col) throws IOException {
+        init();
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Get get = new Get(rowKey.getBytes());
+        get.addColumn(colFamily.getBytes(), col.getBytes());
+        Result result = table.get(get);
+        showCell(result);
+        table.close();
+        close();
+    }
+
+    public static void showCell(Result result){
+        Cell[] cells = result.rawCells();
+        for (Cell cell : cells) {
+            System.out.println("RowName:"+new String(CellUtil.cloneRow(cell)+" "));
+            System.out.println("Timetamp:"+cell.getTimestamp()+" ");
+            System.out.println("column Family:"+new String(CellUtil.cloneFamily(cell))+" ");
+            System.out.println("row Name:"+new String(CellUtil.cloneQualifier(cell))+" ");
+            System.out.println("value:"+new String(CellUtil.cloneValue(cell))+" ");
+        }
+    }
+}
