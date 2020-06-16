891a5e94c016f7668a2950d338f71830ad128371
jast90 <zhangzhiwen91@gmail.com>
2020-06-16 14:50:07 +0800

    update gcp prepare.sh

--- shell/gcp/prepare.sh
+++ shell/gcp/prepare.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# 切换到root用户
-sudo -i
-
 # 临时关闭防火墙
-chkconfig iptables off
+sudo chkconfig iptables off
 # 永久关闭防火墙
-servie iptables stop
+sudo service iptables stop
 
 #临时管理selinux
-setenforce 0
+sudo setenforce 0
 #永久关闭seLinux
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 
 # 修改ssh配置使之可以免密登入
-sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/#PasswordAuthentication no/' /etc/ssh/sshd_config
-sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/ChallengeResponseAuthentication no/#ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/#PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/ChallengeResponseAuthentication no/#ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
 
-# 修改root密码
-echo "root:123456" | chpasswd
+
+# 修改root密码(非交互式修改root用户的密码
+# sudo echo "root:123456" | chpasswd #无法执行
 
 # 重启
-init 6
+#init 6
