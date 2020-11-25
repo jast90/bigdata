de1d8b7de8a17156aedce4c52ad9cfaf7142c9fb
jast90 <zhangzhiwen91@gmail.com>
2020-11-25 10:49:55 +0800

    spring data es search

--- elasticsearch/es-api-sample/src/test/java/cn/jast/esapisample/EsApiSampleApplicationTests.java
+++ elasticsearch/es-api-sample/src/test/java/cn/jast/esapisample/EsApiSampleApplicationTests.java
@@ -4,16 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 
 import cn.jast.esapisample.modal.Post;
 import cn.jast.esapisample.util.Jsons;
@@ -69,5 +75,18 @@ public class EsApiSampleApplicationTests {
         post.setMessage("Hello world");
         post = elasticsearchOperations.save(post);
         System.out.println(Jsons.objectToJSON(post));
+        
+    }
+
+    @Test
+    public void search(){
+        QueryBuilder queryBuilder = QueryBuilders.termQuery("message", "hello");
+        NativeSearchQuery query = new NativeSearchQuery(queryBuilder);
+        SearchHits<Post> hits = elasticsearchOperations.search(query, Post.class);
+        List<SearchHit<Post>> list = hits.getSearchHits();
+        list.forEach(i->{
+            System.out.println(Jsons.objectToJSON(i.getContent()));
+        });
+        
     }
 }
