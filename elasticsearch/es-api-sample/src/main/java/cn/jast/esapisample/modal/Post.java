386accc2e28ae0d0059850b5ebbe219a6f15c072
jast90 <zhangzhiwen91@gmail.com>
2020-11-25 10:19:05 +0800

    通过spring data es提供的ElasticsearchOperations进行操作，其中包含index、doc、search等操作

--- elasticsearch/es-api-sample/src/main/java/cn/jast/esapisample/modal/Post.java
+++ elasticsearch/es-api-sample/src/main/java/cn/jast/esapisample/modal/Post.java
@@ -2,13 +2,33 @@ package cn.jast.esapisample.modal;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+@Document(indexName = "post")
 public class Post {
 
+    @Id
+    private long id;
+    @Field
     private String user;
+    @Field(format = DateFormat.basic_date_time)
     private Date date;
+    @Field
     private String message;
+    @Field(format = DateFormat.basic_date_time)
     private Date updateDate;
 
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getUser() {
         return this.user;
--- elasticsearch/es-api-sample/src/test/java/cn/jast/esapisample/EsApiSampleApplicationTests.java
+++ elasticsearch/es-api-sample/src/test/java/cn/jast/esapisample/EsApiSampleApplicationTests.java
@@ -1,9 +1,73 @@
 package cn.jast.esapisample;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+
+import cn.jast.esapisample.modal.Post;
+import cn.jast.esapisample.util.Jsons;
 
 @SpringBootTest
 public class EsApiSampleApplicationTests {
 
-	
+    @Autowired
+    private RestHighLevelClient highLevelClient;
+    
+    @Autowired
+    private  ElasticsearchOperations elasticsearchOperations;
+
+    
+
+    @Test
+    public void deleteIndex(){
+        assertTrue(elasticsearchOperations.indexOps(Post.class).delete());
+    }
+
+    @Test
+    public void createIndex(){
+        assertTrue(elasticsearchOperations.indexOps(Post.class).create());
+
+        
+    }
+
+    @Test
+    public void existsIndex(){
+        assertTrue(elasticsearchOperations.indexOps(Post.class).exists());
+    }
+
+    @Test
+    public void createDoc() throws IOException {
+        IndexRequest request = new IndexRequest("spring-date");
+        request.id("1");
+        Post post = new Post();
+        post.setDate(new Date());
+        post.setUser("jast");
+        post.setMessage("hello");
+        request.source(Jsons.objectToJSON(post), XContentType.JSON);
+        IndexResponse response = highLevelClient.index(request,RequestOptions.DEFAULT);
+        System.out.println(response);
+
+       
+    }
+
+    @Test
+    public void createDoc1(){
+        Post post = new Post();
+        post.setId(1);
+        post.setUser("jast");
+        post.setMessage("Hello world");
+        post = elasticsearchOperations.save(post);
+        System.out.println(Jsons.objectToJSON(post));
+    }
 }
